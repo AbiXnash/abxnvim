eae6fd5064e4f1f77eed2f9f773092c6b77f867d
AbiXnash <abixnash@gmail.com>
2025-03-05 12:46:57 +0530

    Fix File Tree Keymap

--- init.lua
+++ init.lua
@@ -69,6 +69,22 @@ vim.opt.expandtab = true -- Convert tabs to spaces
 vim.opt.softtabstop = 2 -- Backspace treats 2 spaces as a tab
 vim.opt.smartindent = true -- Auto-indent intelligently
 
+local last_buffer = nil
+
+vim.keymap.set("n", "<leader>e", function()
+	local current_buf = vim.api.nvim_get_current_buf()
+
+	-- If already in netrw or file explorer, switch back to last buffer
+	if last_buffer and vim.bo.filetype == "netrw" then
+		vim.api.nvim_set_current_buf(last_buffer)
+		last_buffer = nil
+	else
+		-- Save current buffer before opening netrw
+		last_buffer = current_buf
+		vim.cmd("e .") -- Open the current directory
+	end
+end, { desc = "Toggle File Explorer" })
+
 -- [[ Basic Autocommands ]]
 
 vim.api.nvim_create_autocmd("TextYankPost", {
--- lua/default/keymaps.lua
+++ lua/default/keymaps.lua
@@ -34,19 +34,3 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
-local last_buffer = nil
-
-vim.keymap.set("n", "<leader>e", function()
-	local current_buf = vim.api.nvim_get_current_buf()
-
-	-- If already in netrw or file explorer, switch back to last buffer
-	if last_buffer and vim.bo.filetype == "netrw" then
-		vim.api.nvim_set_current_buf(last_buffer)
-		last_buffer = nil
-	else
-		-- Save current buffer before opening netrw
-		last_buffer = current_buf
-		vim.cmd("e .") -- Open the current directory
-	end
-end, { desc = "Toggle File Explorer" })
