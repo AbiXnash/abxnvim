768c063b57502f2fa3f3a9f28bcf60ff357a7086
AbiXnash <abixnash@gmail.com>
2025-03-13 21:04:05 +0530

    Added fzf, installed markdown and angular LSP

--- lua/abx/plugins/lsp.lua
+++ lua/abx/plugins/lsp.lua
@@ -185,6 +185,10 @@ return {
 				end,
 			})
 
+			lspconfig.angularls.setup({
+				filetypes = { "typescript", "html", "angular" },
+			})
+
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
