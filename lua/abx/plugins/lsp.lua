c9d969efdb0d7dd16259bb27aa552ee402b16039
AbiXnash <abixnash@gmail.com>
2025-03-30 17:52:43 +0530

    Moving remaps

--- lua/abx/plugins/lsp.lua
+++ lua/abx/plugins/lsp.lua
@@ -24,11 +24,55 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
-			local keymaps = require("abx.keymaps.lsp-map")
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
-					keymaps.setup_keymaps(event)
+					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
+					-- to define small helper and utility functions so you don't have to repeat yourself.
+					--
+					-- In this case, we create a function that lets us more easily define mappings specific
+					-- for LSP related items. It sets the mode, buffer and description for us each time.
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+
+					-- Jump to the definition of the word under your cursor.
+					--  This is where a variable was first declared, or where a function is defined, etc.
+					--  To jump back, press <C-t>.
+					map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
+
+					-- Find references for the word under your cursor.
+					map("gr", require("telescope.builtin").lsp_references, "Goto References")
+
+					-- Jump to the implementation of the word under your cursor.
+					--  Useful when your language has ways of declaring types without an actual implementation.
+					map("gI", require("telescope.builtin").lsp_implementations, "Goto Implementation")
+
+					-- Jump to the type of the word under your cursor.
+					--  Useful when you're not sure what type a variable is and you want to see
+					--  the definition of its *type*, not where it was *defined*.
+					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type Definition")
+
+					-- Fuzzy find all the symbols in your current document.
+					--  Symbols are things like variables, functions, types, etc.
+					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
+
+					-- Fuzzy find all the symbols in your current workspace.
+					--  Similar to document symbols, except searches over your entire project.
+					map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace Symbols")
+
+					-- Rename the variable under your cursor.
+					--  Most Language Servers support renaming across files, etc.
+					map("<leader>rn", vim.lsp.buf.rename, "Rename")
+
+					-- Execute a code action, usually your cursor needs to be on top of an error
+					-- or a suggestion from your LSP for this to activate.
+					map("<leader>ca", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
+
+					-- WARN: This is not Goto Definition, this is Goto Declaration.
+					--  For example, in C this would take you to the header.
+					map("gD", vim.lsp.buf.declaration, "Goto Declaration")
 
 					-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
 					---@param client vim.lsp.Client
@@ -78,6 +122,19 @@ return {
 								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
 							end,
 						})
+					end
+
+					-- The following code creates a keymap to toggle inlay hints in your
+					-- code, if the language server you are using supports them
+					--
+					-- This may be unwanted, since they displace some of your code
+					if
+						client
+						and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
+					then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+						end, "Toggle Inlay Hints")
 					end
 				end,
 			})
--- lua/abx/remap.lua
+++ lua/abx/remap.lua
@@ -1,9 +1,4 @@
-require("abx.keymaps.harpoon-map")
-require("abx.keymaps.netrw-map")
-require("abx.keymaps.telescope-map")
-require("abx.keymaps.lsp-map")
-require("abx.keymaps.trouble-map")
-require("abx.keymaps.comments-map")
+local harpoon = require("harpoon")
 -- Cap Q
 vim.api.nvim_create_user_command("Q", "q", {})
 vim.api.nvim_create_user_command("W", "w", {})
@@ -11,10 +6,10 @@ vim.api.nvim_create_user_command("Wq", "wq", {})
 
 -- <Esc> to exit search mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "J", "<cmd>nop<CR>")
+-- vim.keymap.set("n", "J", "<cmd>nop<CR>")
 
 -- TIP: Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<left>", '<cmd>Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
@@ -39,10 +34,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-s>", ":split<CR>", { noremap = true, silent = true }) -- Ctrl + s for horizontal split
 vim.keymap.set("n", "<C-v>", ":vsplit<CR>", { noremap = true, silent = true }) -- Ctrl + v for vertical split
 
--- Line Navigation
-vim.keymap.set("n", "H", "^", { desc = "Go to first word of the line" })
-vim.keymap.set("n", "L", "g_", { desc = "Go to end of the line (excluding newline)" })
-
 -- Window Switch
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
@@ -53,3 +44,92 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, {
 	desc = "Undo Tree UI",
 })
+
+harpoon:setup() -- Correct Harpoon v2 setup
+
+-- Keybindings
+vim.keymap.set("n", "<leader>ha", function()
+	harpoon:list():add()
+end, { desc = "Harpoon: Add file" })
+vim.keymap.set("n", "<leader>hm", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "Harpoon: Open menu" })
+
+-- Jump between files
+vim.keymap.set("n", "<leader>hh", function()
+	harpoon:list():select(1)
+end, { desc = "Harpoon: Go to file 1" })
+vim.keymap.set("n", "<leader>hj", function()
+	harpoon:list():select(2)
+end, { desc = "Harpoon: Go to file 2" })
+vim.keymap.set("n", "<leader>hk", function()
+	harpoon:list():select(3)
+end, { desc = "Harpoon: Go to file 3" })
+vim.keymap.set("n", "<leader>hl", function()
+	harpoon:list():select(4)
+end, { desc = "Harpoon: Go to file 4" })
+
+-- Cycle through files
+vim.keymap.set("n", "]", function()
+	harpoon:list():next()
+end, { desc = "Harpoon: Next file", silent = true })
+
+vim.keymap.set("n", "[", function()
+	harpoon:list():prev()
+end, { desc = "Harpoon: Previous file", silent = true })
+
+-- [[ File Navigation with netrw ]]
+
+local last_buffer = nil
+
+vim.keymap.set("n", "<leader>e", function()
+	local current_buf = vim.api.nvim_get_current_buf()
+
+	-- If already in netrw or file explorer, switch back to last buffer
+	if last_buffer and vim.bo.filetype == "netrw" then
+		vim.api.nvim_set_current_buf(last_buffer)
+		last_buffer = nil
+	else
+		-- Save current buffer before opening netrw
+		last_buffer = current_buf
+		vim.cmd("e .") -- Open the current directory
+	end
+end, { desc = "Toggle File Explorer" })
+
+-- [[ File Navigation: Left and Right movement disable ]]
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "netrw",
+	callback = function()
+		vim.keymap.set("n", "h", "<NOP>", { buffer = true })
+		vim.keymap.set("n", "l", "<NOP>", { buffer = true })
+	end,
+})
+-- [[ Telescope ]]
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader><leader>", builtin.git_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fs", function()
+	builtin.grep_string({ search = vim.fn.input("Search >> ") })
+end, { desc = "Telescope help tags" })
+-- trouble_keymap.lua (Keymap configurations for Trouble)
+vim.keymap.set("n", "<leader>q", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
+vim.keymap.set(
+	"n",
+	"<leader>xX",
+	"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+	{ desc = "Buffer Diagnostics (Trouble)" }
+)
+vim.keymap.set("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "Symbols (Trouble)" })
+vim.keymap.set(
+	"n",
+	"<leader>cl",
+	"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+	{ desc = "LSP Definitions / references / ... (Trouble)" }
+)
+vim.keymap.set("n", "<leader>xL", "<cmd>Trouble loclist toggle<cr>", { desc = "Location List (Trouble)" })
+vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List (Trouble)" })
