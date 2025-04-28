117a45bc3df37ea9d731bdcd430c499ea440e931
Abinash S <abixnash@gmail.com>
2025-04-28 15:39:35 +0530

    feat: Treesitter initial setup

---
+++ lua/abx/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "vimdoc", "java" },
+
+			sync_install = false,
+			auto_install = false,
+			
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end	
+}
