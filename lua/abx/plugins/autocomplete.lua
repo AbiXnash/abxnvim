d0e3c3e47d13e4ba5727117073bc7305783f908c
Abinash S <abixnash@gmail.com>
2025-04-28 21:14:10 +0530

    fix: Insert suggested variable with Enter

--- lua/abx/plugins/autocomplete.lua
+++ lua/abx/plugins/autocomplete.lua
@@ -3,13 +3,30 @@ return {
     dependencies = 'rafamadriz/friendly-snippets',
     version = "v0.*",
     opts = {
-        keymap = { preset = "default" },
+        keymap = {
+            preset = "default",
+            ['<Esc>'] = { 'hide' },
+        },
 
         appearance = {
             use_nvim_cmp_as_default = true,
-            nerd_font_variant = "mono"
+            nerd_font_variant = "mono",
         },
 
-        signature = { enabled = true, }
+        signature = { enabled = true, },
     },
+
+    config = function(_, opts)
+        require("blink.cmp").setup(opts)
+
+        vim.keymap.set("i", "<CR>", function()
+            local blink_cmp = require("blink.cmp")
+            if blink_cmp.is_visible() then
+                blink_cmp.select_and_accept()
+            else
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, true, true), "n", true)
+            end
+        end, { desc = "Blink Smart Accept or Newline" })
+    end
 }
+
