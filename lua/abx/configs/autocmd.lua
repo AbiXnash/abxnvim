9b3a4c36abbc1b15ef9efca10352a13fb1959682
Abinash S <abixnash@gmail.com>
2025-04-28 18:31:53 +0530

    feat: Integrated format on save

--- lua/abx/configs/autocmd.lua
+++ lua/abx/configs/autocmd.lua
@@ -1,7 +1,24 @@
 -- Highlight on yank!
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("yank-highlight", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    group = vim.api.nvim_create_augroup("yank-highlight", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- Format on save
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then return end
+
+        if client.supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                end
+            })
+        end
+    end,
 })
