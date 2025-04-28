0eb27e00829b5b7bbbbd9e6b075771e1005f7c7f
Abinash S <abixnash@gmail.com>
2025-04-28 13:03:59 +0530

    feat: Setup Lazy.nvim

---
+++ lua/abx/init.lua
@@ -0,0 +1,34 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "http://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazyrepo,
+		lazypath,
+	})
+
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{"Failed to clone!!\n", "ErrorMsg"},
+			{"out", "WarningMsg"},
+			{"\nPress any key to exit..."},
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("abx.configs.options")
+
+require("lazy").setup({
+	spec = {
+		{import = "abx.plugins"}
+	},
+	checker = {enabled = true},
+})
