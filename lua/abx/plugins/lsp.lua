3911d5b096cec7f83ad1eee8b3661eba7c5b6af7
Abinash S <abixnash@gmail.com>
2025-04-28 21:05:37 +0530

    feat: Integrated autocomplete setup

--- lua/abx/plugins/lsp.lua
+++ lua/abx/plugins/lsp.lua
@@ -30,11 +30,11 @@ return {
         },
     },
 
-
     config = function()
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
         require("mason-lspconfig").setup_handlers({
             function(server_name)
-                require("lspconfig")[server_name].setup({})
+                require("lspconfig")[server_name].setup({ capabilities = capabilities })
             end
         })
     end
