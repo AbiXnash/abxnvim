442c3b58522e4f280e762220eebd52193aea7b6a
Abinash S <abixnash@gmail.com>
2025-04-28 18:01:22 +0530

    fix: removed unused variable

--- lua/abx/init.lua
+++ lua/abx/init.lua
@@ -2,7 +2,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "http://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({
+	local _ = vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
