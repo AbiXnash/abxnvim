2f21e2a29450653634e254b80966936c5a8b6e25
Abinash S <abixnash@gmail.com>
2025-04-29 08:05:35 +0530

    feat: Java formatter config

---
+++ lua/abx/plugins/init.lua
@@ -0,0 +1,11 @@
+return {
+    { "folke/todo-comments.nvim", opts = {} },
+    {
+        "mbbill/undotree",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            { "<leader>U", vim.cmd.UndotreeToggle }
+        }
+
+    }
+}
