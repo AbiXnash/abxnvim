f4d58d565ac2818192e631e273e4a150b9969757
Abinash <abixnash@gmail.com>
2025-05-02 20:57:37 +0530

    fix: visual line error fix

--- lua/abx/init.lua
+++ lua/abx/init.lua
@@ -36,3 +36,11 @@ require("lazy").setup({
     checker = { enabled = false },
     change_detection = { notify = false },
 })
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+})
