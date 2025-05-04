6adf81fe321c3a14a355d3744c72e42d8f58bbfc
Abinash <abixnash@gmail.com>
2025-05-04 17:22:33 +0530

    feat: configured comments plugin

--- lua/abx/plugins/comments.lua
+++ lua/abx/plugins/comments.lua
@@ -5,7 +5,28 @@ return {
         dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
         config = function()
             require("Comment").setup({
+                padding = true,
+                sticky = true,
+                ignore = '^$',
+                toggler = {
+                    line = 'gcc',
+                    block = 'gbc',
+                },
+                opleader = {
+                    line = 'gc',
+                    block = 'gb',
+                },
+                extra = {
+                    above = 'gcO',
+                    below = 'gco',
+                    eol = 'gcA',
+                },
+                mappings = {
+                    basic = true,
+                    extra = true,
+                },
                 pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+                post_hook = function() end,
             })
 
             -- Keybindings
