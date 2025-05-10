21ed454b08d3fcd51fb9b841501cad3eec03cff4
Abinash <abixnash@gmail.com>
2025-05-10 22:37:16 +0530

    added git plugins

--- lua/abx/plugins/init.lua
+++ lua/abx/plugins/init.lua
@@ -6,17 +6,37 @@ return {
             { "<leader>U", vim.cmd.UndotreeToggle }
         }
     },
+    {
+        "mistricky/codesnap.nvim",
+        build = "make",
+        keys = {
+            { "<leader>cc", "<cmd>CodeSnap<cr>",     mode = "x", desc = "Save selected code snapshot into clipboard" },
+            { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+        },
+        opts = {
+            save_path = "~/Pictures",
+            has_breadcrumbs = true,
+            watermark = "CodeSnap.nvim",
+            bg_theme = "bamboo",
+        },
+    },
+
+    -- Git
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            current_line_blame = true,
+        }
+    },
+    { "tpope/vim-fugitive" },
+
+    -- CSS
     { 'brenoprata10/nvim-highlight-colors', opts = {} },
 
     -- JSON
     { "b0o/schemastore.nvim" },
 
-    -- Git
-    { "tpope/vim-fugitive" },
-    { "lewis6991/gitsigns.nvim",            opts = {} },
 
     -- Presentaion
     { 'tjdevries/present.nvim' },
-
-    -- Markdown
 }
